ea3fe35977d734f6296b01cb5a228eee1d6b8b1d
DJ-22 <jaindaksh006@gmail.com>
2025-10-31 15:10:37 +0530

    feat(config): add Queens test board with color mappings

---
+++ Queens-SampleCase/config.py
@@ -0,0 +1,59 @@
+"""Sample 16x16 Grid Test Case Configuration for the Queens Problem"""
+
+BOARD = """
+ABBCCCCCCCCCCCCC
+AABBCCCCCCCCCCCC
+AABBCCCCCCCCCCCC
+DABBECCCCCCCCCCF
+DABBECCAACCCCFFF
+DAAAAAAACCCFFFFF
+AAAAAAAAAAGGFFFH
+AAAAAAAAAAGGFIIH
+AAAAAAAAAGGFFFIH
+AAAAJJJAAAKFFFII
+AAAJJJJJJKKKFFLL
+AAAJJJJJJKMKFLLL
+AAJJJNJJOMMMFLLM
+AJJJJNNJOOMMMMMM
+AJJJNNNJOOOOOMMM
+AJJJJNPPPOOOOOOO
+"""
+
+COLOR_MAP = {
+    "A": "silver",
+    "B": "blue",
+    "C": "cyan",
+    "D": "green",
+    "E": "grey",
+    "F": "hazel",
+    "G": "magenta",
+    "H": "orange",
+    "I": "pink",
+    "J": "red",
+    "K": "sage",
+    "L": "turquoise",
+    "M": "purple",
+    "N": "white",
+    "O": "yellow",
+    "P": "lavender"
+}
+
+COLORS = {
+    "purple": (128, 0, 128),
+    "pink": (255, 192, 203),
+    "blue": (0, 0, 255),
+    "grey": (128, 128, 128),
+    "white": (255, 255, 255),
+    "red": (255, 0, 0),
+    "yellow": (255, 255, 0),
+    "cyan": (0, 255, 255),
+    "orange": (255, 170, 80),
+    "black": (0, 0, 0),
+    "turquoise": (64, 224, 208),
+    "magenta": (255, 0, 255),
+    "sage": (178, 172, 136),
+    "hazel": (142, 118, 24),
+    "green": (0, 255, 0),
+    "silver": (192, 192, 192),
+    "lavender": (131, 52, 35)
+}
